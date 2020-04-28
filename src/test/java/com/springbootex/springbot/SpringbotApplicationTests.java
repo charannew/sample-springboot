84fcd0bb302b2b6cd7c5fa04b8d4c5a8ddb9d41b
Charan Kumar <charan.kumar@ecolab.com>
2020-04-28 13:01:53 +0530

    first commit

---
+++ src/test/java/com/springbootex/springbot/SpringbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootex.springbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
